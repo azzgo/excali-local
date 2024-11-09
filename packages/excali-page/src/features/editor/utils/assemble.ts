790d34efac09cb78d767e9804c748807c82c1bbc
ISON <azzgo@users.noreply.github.com>
2024-11-10 00:55:24 +0800

    fix: cachedBlob reduce useless network request

--- packages/excali-page/src/features/editor/utils/assemble.ts
+++ packages/excali-page/src/features/editor/utils/assemble.ts
@@ -6,6 +6,8 @@ import { isFrame } from "./filters";
 import { exportToBlob } from "@excalidraw/excalidraw";
 import { BinaryFiles } from "@excalidraw/excalidraw/types/types";
 
+const cachedThumbnail = new Map<string, string>();
+
 export const assembleSlides = (
   elements: readonly ExcalidrawElement[],
   files: BinaryFiles
@@ -14,18 +16,24 @@ export const assembleSlides = (
 
   return Promise.all(
     frames.map(async (frame, index) => {
-      const thumbnail = await exportToBlob({
-        elements: elements.filter((el) => el.frameId === frame.id),
-        appState: {
-          viewBackgroundColor: "transparent",
-        },
-        files,
-      });
+      let thumbnail = cachedThumbnail.get(frame.id);
+
+      if (!thumbnail) {
+        const blob = await exportToBlob({
+          elements: elements.filter((el) => el.frameId === frame.id),
+          appState: {
+            viewBackgroundColor: "transparent",
+          },
+          files,
+        });
+        thumbnail = URL.createObjectURL(blob);
+        cachedThumbnail.set(frame.id, thumbnail);
+      }
 
       return {
         id: frame.id,
         name: frame.name ?? `Frame ${index + 1}`,
-        thumbnail: URL.createObjectURL(thumbnail),
+        thumbnail,
         element: frame,
       };
     })
