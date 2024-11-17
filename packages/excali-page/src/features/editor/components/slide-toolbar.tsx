f0db19f08f215e31820ff786b43709fbb5bd25d3
ISON <azzgo@users.noreply.github.com>
2024-11-17 10:08:39 +0800

    fix: remove useless code

--- packages/excali-page/src/features/editor/components/slide-toolbar.tsx
+++ packages/excali-page/src/features/editor/components/slide-toolbar.tsx
@@ -2,26 +2,16 @@ import { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types/types";
 import { IconPresentation, IconPresentationOff } from "@tabler/icons-react";
 import { Button } from "@/components/ui/button";
 import { useSlide } from "../hooks/use-slide";
-import { slideGlobalIndexAtom } from "../store/presentation";
-import { useEffect } from "react";
-import { useSetAtom } from "jotai";
 import Hint from "@/components/hint";
 
 interface SlideToolbarProps {
   excalidrawApi: ExcalidrawImperativeAPI | null;
 }
 const SlideToolbar = ({ excalidrawApi }: SlideToolbarProps) => {
-  const updateSlideIndex = useSetAtom(slideGlobalIndexAtom);
   const { presentationMode, slides, handleTogglePresentation } =
     useSlide(excalidrawApi);
-
-  useEffect(() => {
-    if (presentationMode) {
-      updateSlideIndex(0);
-    }
-  }, [presentationMode]);
-
   return (
+
     <div className="flex gap-x-4">
       <Hint
         label={presentationMode ? "Exit Presentation" : "Enter Presentation"}
