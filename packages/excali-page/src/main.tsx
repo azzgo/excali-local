26a89d510406699371d127448138b2827169bf4f
ISON <azzgo@users.noreply.github.com>
2026-01-18 12:20:19 +0800

    fix: local font replacement in option page
    refer to #2

--- packages/excali-page/src/main.tsx
+++ packages/excali-page/src/main.tsx
@@ -6,19 +6,24 @@ import { Toaster } from "sonner";
 import { Provider } from "jotai";
 import { initI18n } from "./locales/locales";
 import { injectCustomFonts } from "./lib/font-injector";
+import { initFontConfig } from "@excalidraw/excalidraw";
 
 initI18n();
 
 // Inject custom fonts before Excalidraw initializes
 // This ensures CSS @font-face rules are available when Excalidraw tries to use fonts
 // Requirements: 1.1, 2.1, 4.1
-injectCustomFonts().then(() => {
+injectCustomFonts().then((config) => {
+  if (config) {
+    initFontConfig({ ...config });
+  }
+
   ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
       <Provider>
         <Editor />
         <Toaster />
       </Provider>
-    </React.StrictMode>
+    </React.StrictMode>,
   );
 });
